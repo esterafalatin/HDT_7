d14570ea2a3977fa86058e2521ba31ea8296c0d1
Daniel <happy@Danny-2.local>
2014-09-10 17:44:56 -0600

    BinarryTree
    a

---
+++ Diccionario/src/diccionario/BinaryTree.java
@@ -0,0 +1,52 @@
+
+public class BinaryTree<E>
+{
+    protected E val; 
+    protected BinaryTree<E> parent; 
+    protected BinaryTree<E> left, right;
+    
+    public BinaryTree(E value, BinaryTree<E> left, BinaryTree<E> right, BinaryTree<E> parent){
+        val=value;
+        this.left=left;
+        this.right=right;
+        this.parent=parent;
+    }
+    
+    public BinaryTree<E> left()
+    {
+            return left;
+    }
+
+    public BinaryTree<E> right()
+    {
+            return right;
+    }
+
+    public BinaryTree<E> parent()
+    {
+        return parent;
+    }
+    
+    public void setLeft(BinaryTree<E> newLeft){
+        this.left=newLeft;
+    }
+    
+    public void setRight(BinaryTree<E> newRight){
+        this.right=newRight;
+    }
+    
+    public void setParent(BinaryTree<E> newParent){
+        this.left=newParent;
+    }
+    
+    public E value()
+	{
+		return val;
+	}
+	
+	public void setValue(E value)
+	{
+		val = value;
+	}
+    
+}
